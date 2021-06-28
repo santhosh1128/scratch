dcb7526d09dc372f46898c9cc5970c3e85a3c3a2
santhosh <83156219+santhosh1128@users.noreply.github.com>
2021-06-28 22:16:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const blockIconURI ='https://static.thenounproject.com/png/967229-200.png';
 class ScratchFetch {
     constructor() {
     }
@@ -6,6 +7,7 @@ class ScratchFetch {
         return {
             "id": "Temperature",
             "name": "Temperature",
+            blockIconURI: blockIconURI
             "blocks": [
                         {
                             "opcode": "fetchURL",
